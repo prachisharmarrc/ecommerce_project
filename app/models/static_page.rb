a161d5b171963e78a17c7797b6594ab933f11107
psharma <psharma8rrc.ca>
2024-07-15 00:43:56 -0500

    added validations for static page model

--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -1,4 +1,5 @@
 class StaticPage < ApplicationRecord
+    validates :title, :content, presence: true
     def self.ransackable_attributes(auth_object = nil)
         
       ["content", "created_at", "id", "title", "updated_at"]
